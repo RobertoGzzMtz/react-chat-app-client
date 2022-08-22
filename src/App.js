d685ddf48d826909b4cca78863a7afd0449f644c
RobertoGzzMtz <robertogzzmtz@gmail.com>
2022-08-22 16:47:11 -0500

    version 1.1 stable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Home from "./components/Home";
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
 
-const socket = socketIO.connect("http://localhost:4000");
+const socket = socketIO.connect(
+  "https://nodejs-chat-app-whatsfake.herokuapp.com/"
+);
 function App() {
   return (
     <BrowserRouter>
